ac47ec78cc1674d55388d2854df98872ab2ebe34
João Paulo <61391556+joaopaulony@users.noreply.github.com>
2021-04-14 18:39:10 -0300

    ADD files
    add files upload

---
+++ Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        AtualizarSerie();
+                        break;
+                    case "4":
+                        ExcluirSerie();
+                        break;
+                    case "5":
+                        VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+
+            Console.WriteLine("╔++++DIO++++++++++++++++++++++++++++++++++++++╗");
+            Console.WriteLine();
+            Console.WriteLine("   Obrigado por utilizar nossos serviços 💜;");
+            Console.WriteLine();
+            Console.WriteLine("╚++++++++++++++++++++++ by: João Paulo CS ++++╝");
+            Console.ReadLine();
+        }
+
+        // Listar Series
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar Séries");
+
+            var lista = repositorio.Lista();
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                var excluido = serie.retornaExcluido();
+
+                Console.WriteLine("#ID {0}: - {1} - {2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "*Excluido*" : ""));
+            }
+        }
+
+        // Inserir Série
+        private static void InserirSerie()
+        {
+            Console.WriteLine("Inserir nova série");
+
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=netcore-3.1
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=netcore-3.1
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Titulo da Série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Inicio da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição da Série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie(id: repositorio.ProximoId(), genero: (Genero)entradaGenero, titulo: entradaTitulo, ano: entradaAno, descricao: entradaDescricao);
+
+            repositorio.Insere(novaSerie);
+        }
+
+        // Atualizar Série
+        private static void AtualizarSerie()
+        {
+            Console.Write("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=netcore-3.1
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=netcore-3.1
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Título da Série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Início da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição da Série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie(id: indiceSerie,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+        }
+
+        // Excluir Série
+        private static void ExcluirSerie()
+        {
+            Console.Write("Digite o id da série que deseja excluir: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+
+        // Visualizar Série
+        private static void VisualizarSerie()
+        {
+            Console.Write("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+
+            Console.WriteLine(serie);
+        }
+
+        // -=======================================-
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("  Loading…");
+            Console.WriteLine("  █▒▒▒▒▒▒▒▒▒");
+            Console.WriteLine("  10%");
+            Console.WriteLine("  ███▒▒▒▒▒▒▒");
+            Console.WriteLine("  30%");
+            Console.WriteLine("  █████▒▒▒▒▒");
+            Console.WriteLine("  50%");
+            Console.WriteLine("  ███████▒▒▒");
+            Console.WriteLine("  100%");
+            Console.WriteLine("  ██████████");
+            Console.WriteLine();
+            Console.WriteLine("╔++++DIO++++++++++++++++++++++++++++++++++++++╗");
+            Console.WriteLine(" JP - Registro de Séries ao seu dispor!!!");
+            Console.WriteLine("   Informe a opção desejada:");
+
+            Console.WriteLine("   [1] Listar séries");
+            Console.WriteLine("   [2] Inserir nova série");
+            Console.WriteLine("   [3] Atualizar série");
+            Console.WriteLine("   [4] Excluir série");
+            Console.WriteLine("   [5] Visualizar série");
+            Console.WriteLine("   [C] Limpar Tela");
+            Console.WriteLine("   [X] Sair");
+            Console.WriteLine("╚++++++++++++++++++++++ by: João Paulo CS ++++╝");
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
